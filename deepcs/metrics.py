4dfff1b52c0916eeb64051abf1440262ac2bbe31
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-01-21 12:41:21 +0100

    allows GenericBatchMetric to accept packedsequences as input

--- deepcs/metrics.py
+++ deepcs/metrics.py
@@ -22,7 +22,10 @@ class GenericBatchMetric:
         targets : (B, *)
         """
         # We suppose is batch averaged
-        B = targets.shape[0]
+        if isinstance(targets, torch.nn.utils.rnn.PackedSequence):
+            B = targets.unsorted_indices.shape[0]
+        else:
+            B = targets.shape[0]
         self.cum_metric += B * self.metric(predictions, targets).item()
         self.num_samples += B
 
